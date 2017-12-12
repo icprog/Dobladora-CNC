e4d6b4c11f494d304bbcec7898b08b5653a6ade6
System-R <System-R@SYSTEM-R-PC>
2017-12-12 14:52:29 -0700

    Modificaciones previas a los estados* *1 se espera un cambio para corregir los estados y las posiciones

--- Controlador/Principal.Designer.cs
+++ Controlador/Principal.Designer.cs
@@ -59,13 +59,15 @@
             this.btn_conectar_lectura = new System.Windows.Forms.Button();
             this.btn_iniciar_captura = new System.Windows.Forms.Button();
             this.bnt_detener_lectura = new System.Windows.Forms.Button();
+            this.lbl_cant_vectores = new System.Windows.Forms.Label();
+            this.lbl_cant_angulos = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // txt_info
             // 
             this.txt_info.Enabled = false;
-            this.txt_info.Location = new System.Drawing.Point(508, 355);
+            this.txt_info.Location = new System.Drawing.Point(633, 332);
             this.txt_info.Margin = new System.Windows.Forms.Padding(2);
             this.txt_info.Name = "txt_info";
             this.txt_info.Size = new System.Drawing.Size(140, 20);
@@ -76,7 +78,7 @@
             this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Yellow;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.Black;
-            this.button1.Location = new System.Drawing.Point(509, 97);
+            this.button1.Location = new System.Drawing.Point(634, 74);
             this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(139, 41);
@@ -120,7 +122,7 @@
             // lb_proceso
             // 
             this.lb_proceso.FormattingEnabled = true;
-            this.lb_proceso.Location = new System.Drawing.Point(508, 152);
+            this.lb_proceso.Location = new System.Drawing.Point(633, 129);
             this.lb_proceso.Margin = new System.Windows.Forms.Padding(2);
             this.lb_proceso.Name = "lb_proceso";
             this.lb_proceso.Size = new System.Drawing.Size(140, 173);
@@ -174,38 +176,39 @@
             // 
             this.lb_transformado.FormattingEnabled = true;
             this.lb_transformado.HorizontalScrollbar = true;
-            this.lb_transformado.Location = new System.Drawing.Point(165, 429);
+            this.lb_transformado.Location = new System.Drawing.Point(7, 507);
             this.lb_transformado.Margin = new System.Windows.Forms.Padding(2);
             this.lb_transformado.Name = "lb_transformado";
-            this.lb_transformado.Size = new System.Drawing.Size(163, 56);
+            this.lb_transformado.Size = new System.Drawing.Size(163, 121);
             this.lb_transformado.TabIndex = 13;
+            this.lb_transformado.SelectedIndexChanged += new System.EventHandler(this.lb_transformado_SelectedIndexChanged);
             // 
             // lb_angulos
             // 
             this.lb_angulos.FormattingEnabled = true;
             this.lb_angulos.HorizontalScrollbar = true;
-            this.lb_angulos.Location = new System.Drawing.Point(491, 430);
+            this.lb_angulos.Location = new System.Drawing.Point(573, 507);
             this.lb_angulos.Margin = new System.Windows.Forms.Padding(2);
             this.lb_angulos.Name = "lb_angulos";
-            this.lb_angulos.Size = new System.Drawing.Size(157, 56);
+            this.lb_angulos.Size = new System.Drawing.Size(215, 121);
             this.lb_angulos.TabIndex = 14;
             // 
             // lb_diferencia
             // 
             this.lb_diferencia.FormattingEnabled = true;
             this.lb_diferencia.HorizontalScrollbar = true;
-            this.lb_diferencia.Location = new System.Drawing.Point(333, 430);
+            this.lb_diferencia.Location = new System.Drawing.Point(195, 507);
             this.lb_diferencia.Name = "lb_diferencia";
-            this.lb_diferencia.Size = new System.Drawing.Size(156, 56);
+            this.lb_diferencia.Size = new System.Drawing.Size(373, 121);
             this.lb_diferencia.TabIndex = 15;
             // 
             // lb_segmentos
             // 
             this.lb_segmentos.FormattingEnabled = true;
-            this.lb_segmentos.Location = new System.Drawing.Point(7, 348);
+            this.lb_segmentos.Location = new System.Drawing.Point(7, 354);
             this.lb_segmentos.Margin = new System.Windows.Forms.Padding(2);
             this.lb_segmentos.Name = "lb_segmentos";
-            this.lb_segmentos.Size = new System.Drawing.Size(154, 134);
+            this.lb_segmentos.Size = new System.Drawing.Size(154, 147);
             this.lb_segmentos.TabIndex = 16;
             // 
             // label3
@@ -223,7 +226,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(8, 325);
+            this.label4.Location = new System.Drawing.Point(8, 329);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(138, 20);
@@ -233,7 +236,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(506, 377);
+            this.label5.Location = new System.Drawing.Point(631, 354);
             this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(126, 13);
@@ -242,7 +245,7 @@
             // 
             // tx_dpp
             // 
-            this.tx_dpp.Location = new System.Drawing.Point(491, 13);
+            this.tx_dpp.Location = new System.Drawing.Point(602, 13);
             this.tx_dpp.Margin = new System.Windows.Forms.Padding(2);
             this.tx_dpp.Name = "tx_dpp";
             this.tx_dpp.Size = new System.Drawing.Size(104, 20);
@@ -250,7 +253,7 @@
             // 
             // btn_dpp
             // 
-            this.btn_dpp.Location = new System.Drawing.Point(600, 12);
+            this.btn_dpp.Location = new System.Drawing.Point(711, 12);
             this.btn_dpp.Margin = new System.Windows.Forms.Padding(2);
             this.btn_dpp.Name = "btn_dpp";
             this.btn_dpp.Size = new System.Drawing.Size(62, 21);
@@ -262,7 +265,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(506, 334);
+            this.label6.Location = new System.Drawing.Point(631, 311);
             this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(43, 13);
@@ -272,7 +275,7 @@
             // txt_dist
             // 
             this.txt_dist.Enabled = false;
-            this.txt_dist.Location = new System.Drawing.Point(508, 393);
+            this.txt_dist.Location = new System.Drawing.Point(633, 370);
             this.txt_dist.Margin = new System.Windows.Forms.Padding(2);
             this.txt_dist.Name = "txt_dist";
             this.txt_dist.Size = new System.Drawing.Size(140, 20);
@@ -285,16 +288,17 @@
             this.pictureBox1.Location = new System.Drawing.Point(170, 148);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(318, 277);
+            this.pictureBox1.Size = new System.Drawing.Size(443, 354);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 24;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(417, 14);
+            this.label7.Location = new System.Drawing.Point(528, 14);
             this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(64, 17);
@@ -303,7 +307,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(393, 42);
+            this.button2.Location = new System.Drawing.Point(357, 27);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(95, 23);
             this.button2.TabIndex = 26;
@@ -351,7 +355,7 @@
             // 
             // bnt_detener_lectura
             // 
-            this.bnt_detener_lectura.Location = new System.Drawing.Point(253, 115);
+            this.bnt_detener_lectura.Location = new System.Drawing.Point(275, 115);
             this.bnt_detener_lectura.Name = "bnt_detener_lectura";
             this.bnt_detener_lectura.Size = new System.Drawing.Size(122, 23);
             this.bnt_detener_lectura.TabIndex = 31;
@@ -359,12 +363,32 @@
             this.bnt_detener_lectura.UseVisualStyleBackColor = true;
             this.bnt_detener_lectura.Click += new System.EventHandler(this.bnt_detener_lectura_Click);
             // 
+            // lbl_cant_vectores
+            // 
+            this.lbl_cant_vectores.AutoSize = true;
+            this.lbl_cant_vectores.Location = new System.Drawing.Point(207, 638);
+            this.lbl_cant_vectores.Name = "lbl_cant_vectores";
+            this.lbl_cant_vectores.Size = new System.Drawing.Size(93, 13);
+            this.lbl_cant_vectores.TabIndex = 32;
+            this.lbl_cant_vectores.Text = "Cantidad vectores";
+            // 
+            // lbl_cant_angulos
+            // 
+            this.lbl_cant_angulos.AutoSize = true;
+            this.lbl_cant_angulos.Location = new System.Drawing.Point(577, 638);
+            this.lbl_cant_angulos.Name = "lbl_cant_angulos";
+            this.lbl_cant_angulos.Size = new System.Drawing.Size(89, 13);
+            this.lbl_cant_angulos.TabIndex = 33;
+            this.lbl_cant_angulos.Text = "Cantidad angulos";
+            // 
             // Principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(660, 500);
+            this.ClientSize = new System.Drawing.Size(799, 663);
+            this.Controls.Add(this.lbl_cant_angulos);
+            this.Controls.Add(this.lbl_cant_vectores);
             this.Controls.Add(this.bnt_detener_lectura);
             this.Controls.Add(this.btn_iniciar_captura);
             this.Controls.Add(this.btn_conectar_lectura);
@@ -437,6 +461,8 @@
         private System.Windows.Forms.Button btn_conectar_lectura;
         private System.Windows.Forms.Button btn_iniciar_captura;
         private System.Windows.Forms.Button bnt_detener_lectura;
+        private System.Windows.Forms.Label lbl_cant_vectores;
+        private System.Windows.Forms.Label lbl_cant_angulos;
     }
 }
 
--- Controlador/Principal.cs
+++ Controlador/Principal.cs
@@ -24,10 +24,16 @@ namespace Controlador
         private List<vector> vectores = new List<vector>();
         private List<vector> depurado = new List<vector>();
         /// <summary>
+        /// Distancia del modulo de ranuradoa las espatulas en el modulo de doblado.
+        /// en milimetros (mm)
+        /// </summary>
+        private const float distancia_espatula=750; 
+        /// <summary>
         /// Distancia minima en la que se agrupan los vetores de una circunferencia
         /// </summary>
         private int resolucion = 20;
         private int distancia_mv = 5;
+        private const double min_v = 1;
         /// <summary>
         /// Variables para compensar la resistencia de la lámina
         /// </summary>
@@ -36,6 +42,7 @@ namespace Controlador
         Graphics dibujo;        
         Pen plumaBlanca = new Pen(Color.White);
         Pen plumaAzul = new Pen(Color.Blue);
+        Pen plumaRoja = new Pen(Color.Red);
         /// <summary>
         /// Esta estructura almacena el punto inicial y final de dos vectores
         /// y la distancia entre ellos
@@ -51,7 +58,8 @@ namespace Controlador
             /// <summary>
             /// 
             /// </summary>
-            public double puntoenlarecta; 
+            public double puntoenlarecta;
+            public bool circunferencia;
         }
         public struct nodo
         {
@@ -206,10 +214,13 @@ namespace Controlador
                 double angulo;
                 vector vanterior = new vector();
                 vector vactual = new vector();
+                bool nvaPosicion = false;
                 while ((linea = archivoGcode.ReadLine()) != null)
                 {
                     if (linea != "")
                     {
+                       // x1 = 0;
+                       // y1 = 0;
                         lb_gcode.Items.Add(linea);
                         patron = "G0Z";//FIN CORTE o CONFIGURACION CORTE
                         busqueda = linea.IndexOf(patron);
@@ -217,28 +228,31 @@ namespace Controlador
                         {
                             planoCorte = false;
                         }
-                        Match bx = Regex.Match(linea, @"X([0-9]+.[0-9]+)\d",
+                        Match bx = Regex.Match(linea, @"X([\-]?[0-9]+.[0-9]+)\d",
                                               RegexOptions.IgnoreCase);
-                        Match by = Regex.Match(linea, @"Y([0-9]+.[0-9]+)\d",
+                        Match by = Regex.Match(linea, @"Y([\-]?[0-9]+.[0-9]+)\d",
                                                                   RegexOptions.IgnoreCase);
                         if (bx.Success)
                         {
+                            nvaPosicion = true;
                             key = bx.Groups[1].Value;
                             if (key != null)
                             {
-                                x1 = Math.Round(Convert.ToDouble(key), 2);
+                                x1 = Math.Round(Convert.ToDouble(key), 3);
                             }
                         }
                         if (by.Success)
                         {
+                            nvaPosicion = true;
                             key = by.Groups[1].Value;
                             if (key != null)
                             {
-                                y1 = Math.Round(Convert.ToDouble(key), 2);
+                                y1 = Math.Round(Convert.ToDouble(key), 3);
                             }
                         }
-                        if (planoCorte)
+                        if (planoCorte && nvaPosicion)
                         {
+                            nvaPosicion = false;
                             nodo n = new nodo();
                             n.x = x1;
                             n.y = y1;
@@ -253,8 +267,8 @@ namespace Controlador
                             inicial.x = x1;
                             inicial.y = y1;
                             lista_nodos.Add(inicial);
-                            anterior = inicial;
-                        }
+                          //  anterior = inicial;
+                        }                        
                     }
                 }
                 vector segmento = new vector();
@@ -263,6 +277,9 @@ namespace Controlador
                 Boolean ini2 = true;
                 distancia = 0;
                 double distancia2 = 0;
+                bool circ=false;
+                //double[] distann = new double[1550];
+                //int cc=0;
                 foreach (nodo n in lista_nodos)
                 {
                     if (ini && ini2)
@@ -282,28 +299,24 @@ namespace Controlador
                         distancia += Math.Sqrt(Math.Pow((anterior.x - preanterior.x), 2) + Math.Pow((anterior.y - preanterior.y), 2));
                         distancia2 = Math.Sqrt(Math.Pow((actual.x - anterior.x), 2) + Math.Pow((actual.y - anterior.y), 2));
 
-                        /*//
-                        Agregar codigo para verificar distancias y angulos y clasificar circunferencias. 
-                        ///*/
-                          //ang_vanterior = Math.Atan2(
-                         //   (vanterior.final.y - vanterior.inicial.y),
-                         //   (vanterior.final.x - vanterior.inicial.x)
-                         //   );
-                        if (distancia2<0.1 && )
+                        if (distancia < 5 && !circ)
                         {
-
+                            circ = true;
                         }
-
-                        /////
+                        
+                        ///Existe una limitante                           
+                        ///Todos los nodos pequeños los toma como circunferencias. 
                         if (distancia2 > distancia_mv || distancia >= resolucion)
                         {
                             segmento.final = anterior;
+                            segmento.circunferencia = circ;
                             segmento.distancia = distancia;
                             distancia = 0;
                             vectores.Add(segmento);
                             segmento_ant = segmento;
                             segmento = new vector();
                             segmento.inicial = anterior;
+                            circ = false;
                         }
                         preanterior = anterior;
                         anterior = actual;
@@ -314,12 +327,14 @@ namespace Controlador
                     distancia += Math.Sqrt(Math.Pow((anterior.x - preanterior.x), 2) + Math.Pow((anterior.y - preanterior.y), 2));
                     segmento.final = anterior;
                     segmento.distancia = distancia;
+                    segmento.circunferencia = circ;
                     vectores.Add(segmento);
                 }
                 else
                 {
                     segmento.final = anterior;
                     segmento.distancia = Math.Sqrt(Math.Pow((anterior.x - preanterior.x), 2) + Math.Pow((anterior.y - preanterior.y), 2));
+                    segmento.circunferencia = circ;
                     vectores.Add(segmento);
                 }
                 ini = true;
@@ -334,7 +349,7 @@ namespace Controlador
                 {
                     dibujo.DrawString(nn.ToString(), drawFont, drawBrush, Convert.ToSingle(ve.final.x) + 40, pictureBox1.Height - Convert.ToSingle(ve.final.y) - 40);
                     nn++;
-                    if (cambio)
+                    /*if (cambio)
                     {
                         dibujo.DrawLine(plumaBlanca, Convert.ToSingle(ve.inicial.x) + 40, pictureBox1.Height - Convert.ToSingle(ve.inicial.y) - 40,
                             Convert.ToSingle(ve.final.x) + 40, pictureBox1.Height - Convert.ToSingle(ve.final.y) - 40);
@@ -345,13 +360,25 @@ namespace Controlador
                         dibujo.DrawLine(plumaAzul, Convert.ToSingle(ve.inicial.x) + 40, pictureBox1.Height - Convert.ToSingle(ve.inicial.y) - 40,
                             Convert.ToSingle(ve.final.x) + 40, pictureBox1.Height - Convert.ToSingle(ve.final.y) - 40);
                         cambio = true;
+                    }*/
+                    if (ve.circunferencia)
+                    {
+                        dibujo.DrawLine(plumaRoja, Convert.ToSingle(ve.inicial.x) + 40, pictureBox1.Height - Convert.ToSingle(ve.inicial.y) - 40,
+                            Convert.ToSingle(ve.final.x) + 40, pictureBox1.Height - Convert.ToSingle(ve.final.y) - 40);
+                        cambio = false;
+                    }
+                    else
+                    {
+                        dibujo.DrawLine(plumaAzul, Convert.ToSingle(ve.inicial.x) + 40, pictureBox1.Height - Convert.ToSingle(ve.inicial.y) - 40,
+                            Convert.ToSingle(ve.final.x) + 40, pictureBox1.Height - Convert.ToSingle(ve.final.y) - 40);
+                        cambio = false;
                     }
                     distancia += ve.distancia;
                     if (ini)
                     {
                         vanterior = ve;
-                        lb_diferencia.Items.Add(ve.distancia);
-                        lb_transformado.Items.Add(String.Format("{{({0},{1}),({2},{3})}}", ve.inicial.x, ve.inicial.y, ve.final.x, ve.final.y));
+                    //    lb_diferencia.Items.Add(ve.distancia);
+                    //    lb_transformado.Items.Add(String.Format("{{({0},{1}),({2},{3})}}", ve.inicial.x, ve.inicial.y, ve.final.x, ve.final.y));
                         ini = false;
                     }
                     else
@@ -395,9 +422,10 @@ namespace Controlador
                             angulo = angulo + 360;
                         }
 
-                        lb_angulos.Items.Add(String.Format("[{0},{1}]:[{2}]::{3}", ang_vanterior, ang_vactual, angulo, nn - 2));
-                        lb_diferencia.Items.Add(vactual.distancia);
-                        lb_transformado.Items.Add(String.Format("{{({0},{1}),({2},{3})}}", vactual.inicial.x, vactual.inicial.y, vactual.final.x, vactual.final.y));
+                        lb_angulos.Items.Add(String.Format("{0}", angulo));
+                        // lb_angulos.Items.Add(String.Format("[{0},{1}]:[{2}]::{3}", ang_vanterior, ang_vactual, angulo, nn - 2));
+                        //  lb_diferencia.Items.Add(vactual.distancia);
+                        //  lb_transformado.Items.Add(String.Format("{{({0},{1}),({2},{3})}}", vactual.inicial.x, vactual.inicial.y, vactual.final.x, vactual.final.y));
                         ///////////////////////////////////////////////////////
                         ////////////INICIA GEERACION DE CODIGO///////////////
                         ///////////////////////////////////////////////////
@@ -436,7 +464,18 @@ namespace Controlador
                 }
                 lb_segmentos.Items.Add(String.Format("C{0}", Math.Round(vanterior.distancia, 2)));
                 txt_dist.Text = distancia.ToString();
+                foreach (nodo no in lista_nodos)
+                {
+                    lb_transformado.Items.Add(String.Format("{0}:{1}",no.x,no.y));
+                }
+                foreach (vector ve in vectores)
+                {
+                    lb_diferencia.Items.Add(String.Format("{0}-{1},{2}-{3}:{4}:{5}",ve.inicial.x,ve.final.x,ve.inicial.y,ve.final.y,ve.distancia,ve.circunferencia));
+                }
+                lbl_cant_angulos.Text = lb_angulos.Items.Count.ToString();
+                lbl_cant_vectores.Text = vectores.Count.ToString();
             }
+
         }
         /// <summary>
         /// Boton de carga de archivos en formato G-code
@@ -537,10 +576,12 @@ namespace Controlador
         private void button2_Click_1(object sender, EventArgs e)
         {
             cb_menu.Items.Clear();
+            cb_menu_lectura.Items.Clear();
             string[] ports = SerialPort.GetPortNames();
             foreach (String st in ports)
             {
                 cb_menu.Items.Add(st);
+                cb_menu_lectura.Items.Add(st);
             }
         }
         /// <summary>
@@ -579,5 +620,16 @@ namespace Controlador
         {
             graficas.Medicion(false);
         }
+
+        private void lb_transformado_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+        
     }    
 }
